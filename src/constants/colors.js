2fc56296ace7353a86f1c4e70273175d5a951eea
QuangAnhNg <d18129855@tudublin.ie>
2022-10-14 18:09:10 +0100

    Default Colors Added

---
+++ src/constants/colors.js
@@ -0,0 +1,9 @@
+
+// main colors to use in project
+const LIGHT_BLUE = '#516fcb';
+const DARK_BLUE = '#465ca0';
+const LIGHT_GREEN = '#59d7cb';
+const LIGHT_PINK = '#f3f3fb';
+const WHITE = '#ffffff';
+
+exports.default = { LIGHT_BLUE, DARK_BLUE, LIGHT_GREEN, LIGHT_PINK, WHITE}
